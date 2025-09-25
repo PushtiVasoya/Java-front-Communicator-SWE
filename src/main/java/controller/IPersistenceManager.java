4e7403580fa549baf893b600bc0986b38671783e
momkingtai <momkingtai@gmail.com>
2025-09-26 01:55:00 +0530

    added some comments

--- src/main/java/controller/IPersistenceManager.java
+++ src/main/java/controller/IPersistenceManager.java
@@ -1,5 +1,8 @@
 package controller;
 
+/**
+ * Manages loading and saving of application data.
+ */
 public interface IPersistenceManager {
     void getChatHistory(String senderEmail);
 
--- src/main/java/controller/IUserState.java
+++ src/main/java/controller/IUserState.java
@@ -1,9 +1,16 @@
 package controller;
 
+/**
+ * Defines the state of the user in the system, handling different actions.
+ */
 public interface IUserState {
     boolean isLoggedIn();
+
     boolean isInstructor();
+
     boolean canDrawOnCanvas();
+
     boolean canUseChat();
+
     String getUserEmail();
 }
